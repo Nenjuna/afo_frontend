06402ed73f758f2ac5e5ef26e5e5d7290266e8b0
Nenjuna <nenjunane@gmail.com>
2023-07-11 17:07:25 +0530

    header changes

--- pages/[language]/[movie].js
+++ pages/[language]/[movie].js
@@ -6,6 +6,11 @@ import MusicCover from "../../components/MusicCover";
 import Songs from "../../components/Songs";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import MoreBy from "../../components/MoreBy";
 
@@ -23,25 +28,42 @@ export const getServerSideProps = async (context) => {
 
 export default function MovieDetails({ movie, musicOther }) {
   const router = useRouter();
-  // console.log(movie);
+  console.log(router);
   const headTitle = `${movie.title} ${movie.language} Songs Download - Album by ${movie.music} | FREE4DOWNLOAD.IN`;
   const headDescription = `${
     movie.title
   } songs download. Starring ${movie.stars.join(", ")}. Music by ${
     movie.music
-  }`;
+  }. You can download ${
+    movie.title
+  } in MP3 format in both 320 and 128 KBPS. High quality music available for streaming and download for free`;
   const headKeywords = `${movie.title} songs download, ${movie.title} by ${movie.music}, ${movie.title} ${movie.language} songs download, ${movie.stars[0]} in ${movie.title} songs download, ${movie.title} mps songs download, ${movie.title} songs play online`;
   const headImg = `https://masstamilan.dev` + movie.img;
+  const incomingSearch = [
+    `${movie.title} ${movie.language} songs free download`,
+    `${movie.title} ${movie.language} songs play online for free`,
+    `${movie.title} songs download 128 kbps`,
+    `${movie.title} songs download 320 kbps`,
+    `${movie.music} songs download`,
+    `${movie.stars[0]} in ${movie.title} songs download`,
+    `${movie.title} album songs download by ${movie.music}`,
+    `${movie.title} ${movie.year} songs download`,
+  ];
+  // const incomingSearch = searchArr.sort(() => Math.random() - 0.5);
   return (
     <>
       <Head>
         <title>{headTitle}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content={headDescription} />
-        <meta name="og:title" content={headDescription} />
+        <meta property="og:site_name" content="Free4Download.in" />
+        <meta property="og:title" content={headTitle} />
+        <meta property="og:description" content={headDescription} />
         <meta name="keywords" content={headKeywords} />
-        <meta name="og:type" content="music.album" />
-        <meta name="og:image" content={headImg} />
+        <meta property="og:type" content="music.album" />
+        <meta property="og:image" content={headImg} />
+        <meta property="og:url" content={window.location.href} />
+        <link href={window.location.href} rel="canonical" />
       </Head>
       <MusicCover movie={movie} />
       <Songs songs={movie.songs} url={movie.url} />
@@ -56,6 +78,11 @@ export default function MovieDetails({ movie, musicOther }) {
           flexGrow: 1,
           paddingLeft: 5,
           paddingBottom: 3,
+          justifyContent: {
+            xs: "center",
+            md: "flex-start",
+          },
+          alignItems: "center",
           maxWidth: {
             xs: "100%",
             md: "70%",
@@ -67,6 +94,32 @@ export default function MovieDetails({ movie, musicOther }) {
         })}
       </Box>
 
+      <Divider variant="middle" />
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          flexWrap: "wrap",
+          flexGrow: 1,
+          paddingLeft: 5,
+          paddingBottom: 3,
+          justifyContent: {
+            xs: "center",
+            md: "flex-start",
+          },
+          alignItems: "center",
+          maxWidth: {
+            xs: "100%",
+            md: "70%",
+          },
+        }}
+      >
+        <List>
+          {incomingSearch.map((search) => (
+            <ListItemText primary={search} key={search}></ListItemText>
+          ))}
+        </List>
+      </Box>
       <Divider variant="middle" />
     </>
   );
